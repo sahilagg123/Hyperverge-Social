cfdc3fe319547e9ba8917e40622c16cee0a40f3f
sahil <aggarwalsahil560@gmail.com>
2021-01-09 12:12:44 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+app.get('/',(req,res)=>{  res.send("yeah") })
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
